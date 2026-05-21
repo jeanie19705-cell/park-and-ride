e0ff56c490594c787be1821082a401b6a0d2354f
Jeanmay1005 <jeanniemarbll@gmail.com>
2026-05-21 19:15:40 +1000

    Initial Commit

---
+++ Park&Ride/Park_RideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Park_RideApp.swift
+//  Park&Ride
+//
+//  Created by Jeannie Huang on 21/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct Park_RideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Park&RideTests/Park_RideTests.swift
@@ -0,0 +1,17 @@
+//
+//  Park_RideTests.swift
+//  Park&RideTests
+//
+//  Created by Jeannie Huang on 21/5/2026.
+//
+
+import Testing
+@testable import Park_Ride
+
+struct Park_RideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
